4c03cfd833f9a3601c95cec5f0df85f38ffc570e
Atataika <egorbi2406@gmail.com>
2018-08-01 01:16:17 +0300

    mockTest

--- src/js/api/mockPhones.js
+++ src/js/api/mockPhones.js
@@ -4,69 +4,69 @@ export default [
         "name": "Pc",
         "price": "2000.00",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "image": "./public/uploads/1.jpg"
+        "image": "../public/uploads/1.jpg"
     },
     {
         "id": "2",
         "name": "PlayStation 4",
         "price": "300.00",
         "description": "Praesent sollicitudin sit amet mi id scelerisque.",
-        "image": "./public/uploads/2.jpg"
+        "image": "../public/uploads/2.jpg"
     },
     {
         "id": "3",
         "name": "Xbox one",
         "price": "300.00",
         "description": "Ut maximus dictum velit vel fringilla.",
-        "image": "./public/uploads/3.jpg"
+        "image": "../public/uploads/3.jpg"
     },
     {
         "id": "4",
         "name": "Asus notebook",
         "price": "400.00",
         "description": "Morbi eu euismod libero. Proin vitae velit sed nulla tristique semper.",
-        "image": "./public/uploads/4.jpg"
+        "image": "../public/uploads/4.jpg"
     },
     {
         "id": "5",
         "name": "Samsung notebook",
         "price": "350.00",
         "description": "Aenean cursus sem at ipsum pretium sagittis.",
-        "image": "./public/uploads/5.jpg"
+        "image": "../public/uploads/5.jpg"
     },
     {
         "id": "6",
         "name": "Samsung Galaxy S6",
         "price": "200.00",
         "description": "Etiam viverra non nibh et egestas.",
-        "image": "./public/uploads/6.jpg"
+        "image": "../public/uploads/6.jpg"
     },
     {
         "id": "7",
         "name": "Samsung Galaxy S7",
         "price": "300.00",
         "description": "Donec arcu enim, semper ac egestas a, mattis nec ligula.",
-        "image": "./public/uploads/7.jpg"
+        "image": "../public/uploads/7.jpg"
     },
     {
         "id": "8",
         "name": "Samsung Galaxy S8",
         "price": "500.00",
         "description": "Proin volutpat, est a consectetur lacinia, felis nibh pharetra turpi.",
-        "image": "./public/uploads/8.jpg"
+        "image": "../public/uploads/8.jpg"
     },
     {
         "id": "9",
         "name": "Samsung Galaxy S9",
         "price": "800.00",
         "description": "Ut sed purus et lacus aliquet placerat sit amet eu nulla.",
-        "image": "./public/uploads/9.jpg"
+        "image": "../public/uploads/9.jpg"
     },
     {
         "id": "10",
         "name": "iPad 2017",
         "price": "450.00",
         "description": "Maecenas placerat in enim id auctor.",
-        "image": "./public/uploads/10.jpg"
+        "image": "../public/uploads/10.jpg"
     }
 ]
